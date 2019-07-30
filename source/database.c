4e81808103280ea5c35b7ee5484f79b5116c67ca
Melanie Plageman <melanieplageman@gmail.com>
2019-07-29 21:14:26 -0700

    Error-handling and asserts plus update TO-DOs

--- source/database.c
+++ source/database.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -12,13 +13,14 @@
 #define NUM_VERSIONS_INIT 100
 
 
-// TODO: improve error handling instead of just exit(1) and do cleanup
 // TODO: thread-safety -- make some stuff single-threaded; also, add atomics and
 // mutexes to stuff that is potentially multi-threaded
 // TODO: make refcount array a vector
 // TODO: only ftruncate periodically and synchronize refcount array
-// TODO: mmap less (it is slow)
+// TODO: mmap less (it is slow). how could I avoid mmapping for a read
+// transaction?
 
+// TODO: check at database creation time if (PAGE_SIZE < sizeof(database_file_t))
 // TODO: if going more than 1page, use binary tree
 
 struct database_file_t
@@ -38,28 +40,32 @@ static size_t get_page_number(off_t offset)
 
 static transaction_t *start_read_transaction(database_t *database)
 {
-	transaction_t *transaction = malloc(sizeof(transaction_t));
+	transaction_t *transaction;
+	if ((transaction = malloc(sizeof(transaction_t))) == NULL)
+		return NULL;
 	transaction->read_page = database->file->active_page;
 
 	database->refcount[transaction->read_page] += 1;
-	transaction->data = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
-			MAP_SHARED, database->fd, get_page_offset(transaction->read_page));
+
+	if ((transaction->data = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
+			MAP_SHARED, database->fd, get_page_offset(transaction->read_page))) == MAP_FAILED)
+		return NULL;
 
 	return transaction;
 }
 
 static void commit_read_transaction(database_t *database, transaction_t *transaction)
 {
-	if (transaction->read_page > database->num_versions)
-		exit(1);
+	assert(transaction->read_page <= database->num_versions);
+
 	database->refcount[transaction->read_page] -= 1;
 	free(transaction);
 }
 
 static void cancel_read_transaction(database_t *database, transaction_t *transaction)
 {
-	if (transaction->read_page > database->num_versions)
-		exit(1);
+	assert(transaction->read_page <= database->num_versions);
+
 	database->refcount[transaction->read_page] -= 1;
 	free(transaction);
 }
@@ -67,9 +73,13 @@ static void cancel_read_transaction(database_t *database, transaction_t *transac
 static transaction_t *start_write_transaction(database_t *database)
 {
 	struct stat st;
-	int r = fstat(database->fd, &st);
+	int r;
+	if ((r = fstat(database->fd, &st)) == -1)
+		return NULL;
 
-	transaction_t *transaction = malloc(sizeof(transaction_t));
+	transaction_t *transaction;
+	if ((transaction = malloc(sizeof(transaction_t))) == NULL)
+		return NULL;
 	transaction->write_page = SIZE_MAX;
 	for (size_t i = 0; i < database->num_versions; i++)
 	{
@@ -83,20 +93,23 @@ static transaction_t *start_write_transaction(database_t *database)
 	if (transaction->write_page == SIZE_MAX)
 	{
 		/* no available pages. resize the file to have a new page */
-		ftruncate(database->fd, st.st_size + PAGE_SIZE);
+		if ((ftruncate(database->fd, st.st_size + PAGE_SIZE)) == -1)
+			return NULL;
 		transaction->write_page = get_page_number(st.st_size);
 		/* if page number is > 100, will need to resize refcount array */
 		if (transaction->write_page > database->num_versions)
 		{
 			database->num_versions += NUM_VERSIONS_INIT;
-			database->refcount = realloc(database->refcount, database->num_versions);
+			if ((database->refcount = realloc(database->refcount, database->num_versions)) == NULL)
+				return NULL;
 		}
 	}
 	
 	off_t offset = get_page_offset(transaction->write_page);
 
-	transaction->data = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
-			MAP_SHARED, database->fd, offset);
+	if ((transaction->data = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
+			MAP_SHARED, database->fd, offset)) == MAP_FAILED)
+		return NULL;
 
 	return transaction;
 }
@@ -119,14 +132,18 @@ static transaction_t *start_read_write_transaction(database_t *database)
 	// I need it?
 	size_t active_page_num = database->file->active_page;
 	off_t active_page_offset = get_page_offset(active_page_num);
-	char *active_page_addr = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
-			MAP_SHARED, database->fd, active_page_offset);
+	char *active_page_addr;
+	if ((active_page_addr = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
+			MAP_SHARED, database->fd, active_page_offset)) == MAP_FAILED)
+		return NULL;
 	database->refcount[active_page_num] += 1;
 
 	transaction_t *transaction = start_write_transaction(database);
 	memcpy((char *)transaction->data, active_page_addr, PAGE_SIZE);
 	transaction->read_page = active_page_num;
-	int r = munmap(active_page_addr, PAGE_SIZE);
+	int r;
+	if ((r = munmap(active_page_addr, PAGE_SIZE)) == -1)
+		return NULL;
 	return transaction;
 }
 
@@ -134,46 +151,42 @@ static void commit_read_write_transaction(database_t *database, transaction_t *t
 {
 	database->file->active_page = transaction->write_page;
 
-	if (transaction->read_page > database->num_versions)
-		exit(1);
+	assert(transaction->read_page <= database->num_versions);
 	database->refcount[transaction->read_page] -= 1;
 	free(transaction);
 }
 
 static void cancel_read_write_transaction(database_t *database, transaction_t *transaction)
 {
-	if (transaction->read_page > database->num_versions)
-		exit(1);
+	assert(transaction->read_page <= database->num_versions);
+
 	database->refcount[transaction->read_page] -= 1;
 	free(transaction);
 }
 
 database_t *database_new(char *filename)
 {
-	if (PAGE_SIZE < sizeof(database_file_t))
-		exit(1);
-
-	database_t *database = malloc(sizeof(database_t));
+	database_t *database;
+	if ((database = malloc(sizeof(database_t))) == NULL)
+		return NULL;
 	database->num_versions = NUM_VERSIONS_INIT;
-	database->refcount = malloc(sizeof(int) * database->num_versions);
+	if ((database->refcount = malloc(sizeof(int) * database->num_versions)) == NULL)
+		return NULL;
 	for (size_t i = 0; i < database->num_versions; i++)
 	{
 		database->refcount[i] = 0;
 	}
 
-	int fd = open(filename, O_RDWR | O_CREAT, 0666);
-	if (fd < 0)
-		exit(1);
-	database->fd = fd;
+	if ((database->fd = open(filename, O_RDWR | O_CREAT, 0666)) == -1)
+		return NULL;
 
-    int r = ftruncate(database->fd, PAGE_SIZE * 2);
-	if (r == -1)
-		exit(1);
+    int r;
+	if ((r = ftruncate(database->fd, PAGE_SIZE * 2)) == -1)
+		return NULL;
 
-	database->file = mmap(NULL, PAGE_SIZE * 2, PROT_READ | PROT_WRITE,
-			MAP_SHARED, database->fd, 0);
-	if (database->file == MAP_FAILED)
-		exit(1);
+	if ((database->file = mmap(NULL, PAGE_SIZE * 2, PROT_READ | PROT_WRITE,
+			MAP_SHARED, database->fd, 0)) == MAP_FAILED)
+		return NULL;
 
 	database->file->active_page = 1;
 
@@ -184,7 +197,9 @@ database_t *database_new(char *filename)
 
 void database_close(database_t *database)
 {
-	int r = munmap(database, PAGE_SIZE);
+	int r;
+	if ((r = munmap(database, PAGE_SIZE)) == -1)
+		return;
     printf("munmap(buf1): %i\n", r);
 	// XXX: do I need to explicitly free each member of the refcount array?
 	free(database);
